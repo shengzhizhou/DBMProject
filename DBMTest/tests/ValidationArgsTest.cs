76d1af1270571231396772face86cf766f04d51f
shengzhizhou <40223156+shengzhizhou@users.noreply.github.com>
2019-08-05 22:08:01 -0400

    FixBugs

--- DBMTest/tests/ValidationArgsTest.cs
+++ DBMTest/tests/ValidationArgsTest.cs
@@ -10,24 +10,6 @@ namespace DBMTest.tests
     {
         readonly bool InvalidResult = false;
         readonly bool ValidResult = true;
-        [Fact]
-        public void IsValidConn_CorrectConnString_ValidResult()
-        {
-            Options options = new Options
-            {
-                ConnString = "Data source=US-NY-8W1RQ32;Initial Catalog=Version_test;Integrated Security=True;"
-            };
-            Assert.Equal(ValidResult,options.IsValidConn());
-        }
-        [Fact]
-        public void IsValidConn_IncorrectDataSource_InvalidResult()
-        {
-            Options options = new Options
-            {
-                ConnString = "Data source=US-NY-8W1RQ;Initial Catalog=Version_test;Integrated Security=True;"
-            };
-            Assert.Equal(InvalidResult,options.IsValidConn());
-        }
 
         [Fact]
         public void IsValidPath_CorrectLocalPath_ValidResult()
